a9aef4ac3f56805dfbfebcb81e8f7264f7bb59b2
jadchebly <159762508+jadchebly@users.noreply.github.com>
2026-04-14 20:23:51 +0200

    Fix mobile nav and social pill behavior on mobile

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { App } from "./app/App";
 import "./styles/globals.css";
 import "./styles/responsive.css";
+import "./keyboard";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
